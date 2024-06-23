801c20db84d34f0ee7382b3935cf50cb663ca0c1
Renato Filho <renatosousafilho@gmail.com>
2024-06-22 23:32:22 -0300

    setup: cria o arquivo database/sequelize.config.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Cat } from './cats/cat.model';
 import { CatsModule } from './cats/cats.module';
+import { databaseConfig } from './database/sequelize.config';
 
 @Module({
-  imports: [
-    SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      storage: ':memory:',
-      models: [Cat],
-      autoLoadModels: true,
-      synchronize: true,
-    }),
-    CatsModule,
-  ],
+  imports: [SequelizeModule.forRoot(databaseConfig), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
